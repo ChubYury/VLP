10a15d9563a6fe47f619733083171877a33d08b0
ChubYury <gigikalo57@gmail.com>
2023-02-08 08:50:59 +0200

    lesson22: v-on

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const app = Vue.createApp({
- data() {
-  return {
-    userName: 'Jon',
-    userAge: 10,
-    favPhotoLink: 'https://p16.tiktokcdn-us.com/tos-useast5-avt-0068-tx/692de2e0d56771d619114b56e3eb9ba4~c5_720x720.jpeg'
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add() { this.counter++ },
+    
+    reduce() { this.counter > 0 ? this.counter-- : false }
   }
- },
- methods: {
-  nextAge() {return this.userAge + 5},
+})
 
-  favNumber() {return Math.random()}
- }
-});
-
-app.mount('#assignment')
+app.mount('#events')
