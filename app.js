630d12c076691a58e39672c05ced8a90c9bf2091
ChubYury <gigikalo57@gmail.com>
2023-02-10 23:41:35 +0200

    lesson23: event modifiers

--- app.js
+++ app.js
@@ -3,18 +3,19 @@ const app = Vue.createApp({
     return {
       counter: 0,
       userName: '',
+      confirmedName: '',
     };
   },
   methods: {
     add(num) { this.counter += num },
     
-    reduce(num) { 
-      this.counter > 0 ? this.counter -= num : this.counter = 0;
-    },
+    reduce(num) { this.counter > 0 ? this.counter -= num : this.counter = 0 },
 
-    setName(event, lastName) {
-      this.userName = event.target.value +' '+ lastName;
-    }
+    setName(event, lastName) { this.userName = event.target.value +' '+ lastName },
+
+    submitForm() { console.log('Submited!') },
+
+    confirmInp() { this.confirmedName = this.userName },
   }
 })
 
