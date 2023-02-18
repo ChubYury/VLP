9f57a9eaa65c9bb84b0eb3e54905f7ed57656a00
ChubYury <yurichubuser@gmail.com>
2023-02-18 20:39:12 +0200

    lesson70:  understanding the problem

--- app.js
+++ app.js
@@ -1,83 +1,21 @@
 const app = Vue.createApp({
   data() {
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
+      friends: [
+        {id: 'friend001', name: 'Manuel Lorenz', phone: '01234 5678 991', email: 'manuel@localhost.com' },
+        {id: 'friend002', name: 'Julie Jones', phone: '09876 543 221', email: 'julie@localhost.com'},
+      ],
+      flagHide: false,
+    }
   },
+
+  watch: {},
+
+  computed: {},
+
   methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
-    setText() {
-      // this.message = this.currentUserInput;
-      this.message = this.$refs.userText.value;
-      console.dir(this.$refs.userText);
-    },
-  },
-
-  // Starting create app Vue
-  beforeCreate() {console.log('beforCreate()')},
-
-  // Finished creating the app Vue
-  created() {console.log('created()')},
-
-  // Starting mount the app Vue
-  beforeMount() {console.log('beforMount()')},
-
-  // Finished mount the app Vue 
-  mounted() {console.log('mounted()');},
-
-  // Starting element update in the app Vue
-  beforeUpdate() {console.log('beforeUpdate()');},
-
-  // Finished element update in the app Vue
-  updated() {console.log('updated()');},
-
-  // Starting unmount the app Vue
-  beforeUnmount() {console.log('beforUnmount()')},
-  
-  // Finished unmount the app Vue 
-  unmounted() {console.log('unmounted()');},
+    toggleVisible() {this.flagHide = !this.flagHide},
+  }
 });
 
 app.mount('#app');
-
-// setTimeout(function () { 
-  // Delet app Vue
-  // app.unmount('#app')
-// }, 3000)
-
-const app2 = Vue.createApp({
-  template: `
-    <h2>How create two app</h2>
-    <p>{{ favoriteMeal }}</p>
-  `,
-  data() {
-    return {
-      favoriteMeal: 'Pizza'
-    }
-  }
-})
-
-app2.mount('#app2')
-
-// const data = {
-//   message: 'Hello!',
-//   longMessage: 'Hello! World!'
-// };
-
-// const handler = {
-//   set(target, key, value) {
-//     if (key === 'message') {
-//       target.longMessage = value + ' World!';
-//     }
-//     target.message = value;
-//   }
-// };
-
-// const proxy = new Proxy(data, handler);
-
-// proxy.message = 'Hello!!!!';
-
-// console.log(proxy.longMessage);
