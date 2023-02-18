82db19667a8fa4e1706d7b5beb556c413cf7245d
ChubYury <yurichubuser@gmail.com>
2023-02-18 21:24:08 +0200

    lesson71:  firt component

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
         {id: 'friend001', name: 'Manuel Lorenz', phone: '01234 5678 991', email: 'manuel@localhost.com' },
         {id: 'friend002', name: 'Julie Jones', phone: '09876 543 221', email: 'julie@localhost.com'},
       ],
-      flagHide: false,
+      // flagHide: false,
     }
   },
 
@@ -13,9 +13,37 @@ const app = Vue.createApp({
 
   computed: {},
 
+  // methods: {
+  //   toggleVisible() {this.flagHide = !this.flagHide},
+  // }
+});
+
+app.component('user-contact', {
+  template: `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleVisible">{{flagHide ? 'Hide' : 'Show'}} Details</button>
+      <ul v-if="flagHide">
+        <li><strong>Phone: </strong>{{ friend.phone }}</li>
+        <li><strong>Email: </strong>{{ friend.email }}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      flagHide: false,
+      friend: {
+        id: 'friend001', 
+        name: 'Manuel Lorenz', 
+        phone: '01234 5678 991', 
+        email: 'manuel@localhost.com' 
+      }
+    }
+  },
+  
   methods: {
     toggleVisible() {this.flagHide = !this.flagHide},
-  }
-});
+  },
+})
 
 app.mount('#app');
