e02111a5db4b65ebd329759cbe74cbd2edbc8b03
ChubYury <yurichubuser@gmail.com>
2023-02-19 12:37:23 +0200

    lesson88: small addition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <section>
+  <header>
     <h2>My friends</h2>
-
+  </header>
+  <section>
     <ul>
       <user-contact></user-contact>
       <user-contact></user-contact>
@@ -14,9 +15,9 @@
     data() {
       return {
         friends: [
-        {id: 'friend001', name: 'Manuel Lorenz', phone: '01234 5678 991', email: 'manuel@localhost.com' },
-        {id: 'friend002', name: 'Julie Jones', phone: '09876 543 221', email: 'julie@localhost.com'},
-      ],
+          {id: 'friend001', name: 'Manuel Lorenz', phone: '01234 5678 991', email: 'manuel@localhost.com' },
+          {id: 'friend002', name: 'Julie Jones', phone: '09876 543 221', email: 'julie@localhost.com'},
+        ],
       };
     },
     
@@ -25,3 +26,9 @@
     method: {}
   }
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+
+  @import url('./styles.css');
+</style>
