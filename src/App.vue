a53e848c1c655be001a5d14fc9ebd0db91762d7b
ChubYury <yurichubuser@gmail.com>
2023-02-21 14:35:20 +0200

    module8: task1

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,33 @@
 <template>
-  <header>
-    <h2>My friends</h2>
-  </header>
-  <section>
-    <new-user 
-      :count-user="friends.length"
-      @set-user="addNewUser"
-    ></new-user>
-  </section>
-  <section>
-    <ul>
-      <user-contact
-        v-for="friend in friends"
-        :key="friend.id"
-        :id="friend.id"
-        :name="friend.name"
-        :phone-num="friend.phone"
-        :email-addr="friend.email"
-        :is-favorite= "friend.isFavorite"
-        @toggle-favorite="favoriteStatus"
-        @del-user="delUser"
-      ></user-contact>
-    </ul>
-  </section>
+  <header><h1>My first Vue app</h1></header>
+  <user-data
+    @set-data="getData"
+  ></user-data>
+  <active-user
+    :data-name="userName"
+    :data-age="userAge"
+  ></active-user>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        friends: [
-          {id: 'friend1', name: 'Manuel Lorenz', phone: '01234 5678 991', email: 'manuel@localhost.com', isFavorite: true},
-          {id: 'friend2', name: 'Julie Jones', phone: '09876 543 221', email: 'julie@localhost.com', isFavorite: false},
-        ],
-      };
+        userName: '',
+        userAge: ''
+      }
     },
+
     methods: {
-      favoriteStatus(userId) {
-        const userActive = this.friends.find((friend) => friend.id === userId);
-        
-        userActive.isFavorite = !userActive.isFavorite
-      },
-
-      addNewUser(newUser) {this.friends.push(newUser)},
-
-      delUser(idUser) {
-        this.friends = this.friends.filter((friend) => friend.id !== idUser)
+      getData(name, age) {
+        console.log(name, age);
+        this.userName = name;
+        this.userAge = age;
       }
     }
   }
 </script>
 
 <style>
-  @import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
-
   @import url('./styles.css');
 </style>
---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <h1> Show data</h1>
+    <h2><strong>Name:</strong>{{ dataName }}</h2>
+    <h3><strong>Age:</strong>{{ dataAge }}</h3>
+  </section>
+</template>
+
+<script>
+  export default {
+    props: {
+      dataName: {
+        type: String,
+        required: true
+      },
+      dataAge: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
