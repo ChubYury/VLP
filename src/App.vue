6d90f1f4e71cde4e24edc5eb14e7bcab003e75be
ChubYury <yurichubuser@gmail.com>
2023-02-21 16:20:05 +0200

    lesson102: a potential problem

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
-  <header><h1>My first Vue app</h1></header>
-  <user-data
-    @set-data="getData"
-  ></user-data>
-  <active-user
-    :data-name="userName"
-    :data-age="userAge"
-  ></active-user>
+  <div>
+    <active-element
+      :topic-title="activeTopic && activeTopic.title"
+      :text="activeTopic && activeTopic.fullText"
+    ></active-element>
+    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+  </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        userName: '',
-        userAge: ''
-      }
+export default {
+  data() {
+    return {
+      topics: [
+        {
+          id: 'basics',
+          title: 'The Basics',
+          description: 'Core Vue basics you have to know',
+          fullText:
+            'Vue is a great framework and it has a couple of key concepts: Data binding, events, components and reactivity - that should tell you something!',
+        },
+        {
+          id: 'components',
+          title: 'Components',
+          description:
+            'Components are a core concept for building Vue UIs and apps',
+          fullText:
+            'With components, you can split logic (and markup) into separate building blocks and then combine those building blocks (and re-use them) to build powerful user interfaces.',
+        },
+      ],
+      activeTopic: null,
+    };
+  },
+  methods: {
+    activateTopic(topicId) {
+      this.activeTopic = this.topics.find((topic) => topic.id === topicId);
     },
-
-    methods: {
-      getData(name, age) {
-        console.log(name, age);
-        this.userName = name;
-        this.userAge = age;
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style>
