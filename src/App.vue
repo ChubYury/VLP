f8758ec26102aad6e0f9fb27611bbe3fdb75bbd0
ChubYury <yurichubuser@gmail.com>
2023-02-22 13:30:47 +0200

    lesson116: scoped slot

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,25 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <base-card>
+      <cours-goals #default="slotProps">
+        <!-- <template #default="slotProps"> -->
+          <h2>{{ slotProps.item }}</h2>
+          <p>{{ slotProps.anotherProp }}</p>
+        <!-- </template> -->
+      </cours-goals>
+    </base-card>
   </div>
 </template>
 
 <script>
 import TheHeader from './components/TheHeader.vue';
+import CoursGoals from './components/CoursGoals'
 import BadgeList from './components/BadgeList.vue'
 import UserInfo from './components/UserInfo.vue'
 
 export default {
-  components: {TheHeader, BadgeList, UserInfo},
+  components: {TheHeader, BadgeList, UserInfo , CoursGoals},
   
   data() {
     return {
