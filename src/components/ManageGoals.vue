f730b906e86eda2b51081708c98a7f9980f379c1
ChubYury <yurichubuser@gmail.com>
2023-02-22 16:24:49 +0200

    lesson119: dialog window

--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,4 +1,34 @@
 <template>
   <h2>Manage goals</h2>
-  <input type="text">
+  <input type="text" ref="goal">
+  <button @click="setGoal">Set goal</button>
+  <error-alert v-if="inputIsValid">
+    <h2>Input is invalid</h2>
+    <p>Please enter at least a few charecters...</p>
+    <button @click="confirmError">Ok</button>
+  </error-alert>
 </template>
+
+<script>
+  import ErrorAlert from '../components/ErrorAlert.vue';
+
+  export default {
+    components: {ErrorAlert},
+    data() {
+      return {
+        inputIsValid: false,
+      }
+    },
+    methods: {
+      setGoal() {
+        const valueInp = this.$refs.goal.value;
+        if (valueInp === '') {
+          this.inputIsValid = true;
+        }
+      },
+      confirmError() {
+        this.inputIsValid = false;
+      }
+    },
+  };
+</script>
