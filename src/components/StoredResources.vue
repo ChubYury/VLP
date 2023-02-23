4f9e3ebcd786fad2313032c8297bcb09ed44014c
ChubYury <yurichubuser@gmail.com>
2023-02-23 13:31:43 +0200

    lesson129: sltyling and more components

---
+++ src/components/StoredResources.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <ul>
+      <!-- <learning-resorce></learning-resorce> -->
+      <learning-resource 
+      v-for="resource in resourcesLs" 
+      :item="resource"
+      :key="resource.id"
+    ></learning-resource>
+    </ul>
+  </section>
+</template>
+
+<script>
+  import LearningResource from './learning-resources/LearningResource.vue'
+  
+  export default {
+    components: {LearningResource},
+    props: ['resourcesLs'],
+    data() {
+      return {
+
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+  }
+</style>
