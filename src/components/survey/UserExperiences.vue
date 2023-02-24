0e4bc95ef2c58418f1c9efd90961ee201400eaf1
ChubYury <yurichubuser@gmail.com>
2023-02-25 00:12:51 +0200

    lesson157: sending a get request

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
@@ -21,10 +21,23 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  // props: ['results'],
   components: {
     SurveyResult,
   },
+  data() {
+    return {
+      results: [],
+    }
+  },
+
+  methods: {
+    loadExperiences() {
+      fetch('http://localhost:3020/surveys')
+      .then((response) => { if (response.ok) return response.json() })
+      .then((data) => this.results = data)
+    }
+  }
 };
 </script>
 
