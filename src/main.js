5c77f33821967b7683f32e4980029438f03dd145
ChubYury <yurichubuser@gmail.com>
2023-02-26 19:20:19 +0200

    lesson178: using nestes routing

--- src/main.js
+++ src/main.js
@@ -11,10 +11,15 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {path: '/', redirect: '/teams'},
-    { path: '/teams', component: TeamsList},
+    { 
+      path: '/teams', 
+      component: TeamsList,
+      children: [
+        { path: ':teamId', component: TeamMembers, props: true }, // /teams/t1
+      ]
+    },
     // { path: '/teams', component: TeamsList, alias: '/'},
     { path: '/users', component: UsersList },
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
     { path: '/:notFound(.*)', component: NotFound }
     // { path: '/:notFound(.*)', redirect: '/teams'}
   ]
