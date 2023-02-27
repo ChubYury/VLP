283199b6effbc67ccfda6d6e33aed4d0fd27951b
ChubYury <yurichubuser@gmail.com>
2023-02-27 10:12:26 +0200

    lesson191:  start 13 module

--- src/components/teams/TeamsFooter.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h2>Teams footer</h2>
-</template>
--- src/pages/NotFound.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <section>
-    <h2>Page not found!</h2>
-    <p>Maybe view our <router-link to="/teams">Teams</router-link></p>
-  </section>
-</template>
