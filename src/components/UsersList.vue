671983f536b91c3672d55379299a54af92ee74da
ChubYury <yurichubuser@gmail.com>
2023-02-28 16:37:18 +0200

    lesson204: get list for animate

---
+++ src/components/UsersList.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul>
+    <li 
+    v-for="(user, index) in users" 
+    :key="user"
+    @click="delUser(index)"
+    >{{ user }}</li>
+  </ul>
+  <div>
+    <input type="text" name="" id="userInp" v-model="userInpVal">
+    <button @click="addUser"> Add user</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        users: ['Max', 'Menu', 'Julie', 'Angela', 'Michael'],
+        userInpVal: '',
+      }
+    },
+    methods: {
+      addUser() {
+        const newUser = this.userInpVal;
+        this.users.push(newUser);
+      },
+      delUser(idxUser) {
+        this.users.splice(idxUser, 1);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  ul{
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    border: 1px solid #ccc;
+    padding: 1rem;
+    text-align: center;
+  }
+</style>
