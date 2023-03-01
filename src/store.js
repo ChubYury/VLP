0760c937f5f48a36859f27dd406e0b41245b6f2b
ChubYury <yurichubuser@gmail.com>
2023-03-01 14:26:40 +0200

    lesson221: organizing your store with modules

--- src/store.js
+++ src/store.js
@@ -1,63 +1,13 @@
 import { createStore } from 'vuex'
+import { counterModule } from './storag/CounterModule.js'
+import { authModule } from './storag/AuthModule.js'
 
 const store = createStore({
-  state() {
-    return {
-      counter: 0,
-      userAuthValid: false,
-    };
+  modules: {
+    numbers: counterModule,
+    auth: authModule
   },
-  mutations: {
-    increment(state) {
-      state.counter++;
-      // console.log(state);
-    },
-    increase(state, payload) {
-      state.counter += payload.value;
-    },
-    setAuth(state, payload) {
-      state.userAuthValid = payload.isAuth;
-    },
-  },
-  actions: {
-    increment(context) {
-      setTimeout(() => {
-        context.commit('increment')
-      },2000)
-    },
-    increase(context, payload) {
-      console.log('Context',context)
-      context.commit({
-        type: 'increase',
-        value: payload.value
-      })
-    },
-    login(context) {
-      context.commit('setAuth', {isAuth: true})
-    },
-    logout(context) {
-      context.commit('setAuth', {isAuth: false})
-    },
-  },
-  getters: {
-    finalCounter(state) {
-      return state.counter * 2;
-    },
-    normalizedCounter(_state, getters) {
-      const finalCounter = getters.finalCounter;
-
-      if (finalCounter < 0) {
-        return 0;
-      } else if (finalCounter > 100) {
-        return 100;
-      } else {
-        return finalCounter
-      }
-    },
-    userAuthVisible(state) {
-      return state.userAuthValid
-    }
-  }
+  
 })
 
 export default store
