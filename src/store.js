0092a71a7b6c6e4025642877ed167853e3f3e010
ChubYury <yurichubuser@gmail.com>
2023-03-01 10:46:20 +0200

    lesson216: intro getters

--- src/store.js
+++ src/store.js
@@ -14,6 +14,22 @@ const store = createStore({
     increase(state, payload) {
       state.counter += payload.value;
     }
+  },
+  getters: {
+    finalCounter(state) {
+      return state.counter * 2;
+    },
+    normalizedCounter(_state, getters) {
+      const finalCounter = getters.finalCounter;
+
+      if (finalCounter < 0) {
+        return 0;
+      } else if (finalCounter > 100) {
+        return 100;
+      } else {
+        return finalCounter
+      }
+    }
   }
 })
 
